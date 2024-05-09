ec20cbefce5f979e44e0ebef5f762c776665fc4f
airwearpair <airwear.pair@gmail.com>
2024-05-09 08:53:50 +0200

    Thu May  9 08:53:49 CEST 2024

--- index.js
+++ index.js
@@ -63,3 +63,7 @@ const onClickOutside = (elementId, callback) => {
     if (!element.contains(e.target)) callback();
   });
 };
+const isRGBColor = (rgb) => {
+  const regex = /rgb\(\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*,\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*,\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*\)/;
+  return regex.test(rgb);
+}
