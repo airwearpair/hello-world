264e22e05a099fd4ca4369cb76c4b934c8a3b126
airwearpair <airwear.pair@gmail.com>
2024-05-24 06:50:20 +0200

    Fri May 24 06:50:20 CEST 2024

--- index.js
+++ index.js
@@ -131,3 +131,14 @@ const randomString = (length) => {
 function reverseString(str) {
   return str.split("").reverse().join("");
 }
+function countdownTimer(minutes) {
+  let seconds = minutes * 60;
+  const countdown = setInterval(function() {
+    seconds--;
+    if (seconds < 0) {
+      clearInterval(countdown);
+    } else {
+      console.log(seconds + " seconds left");
+    }
+  }, 1000);
+}
