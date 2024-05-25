b259ba7981a87b8c698c2eddeebafe9b39b99fe3
airwearpair <airwear.pair@gmail.com>
2024-05-25 07:17:42 +0200

    Sat May 25 07:17:42 CEST 2024

--- index.js
+++ index.js
@@ -142,3 +142,14 @@ function countdownTimer(minutes) {
     }
   }, 1000);
 }
+function countdownTimer(minutes) {
+  let seconds = minutes * 60;
+  const countdown = setInterval(function() {
+    seconds--;
+    if (seconds < 0) {
+      clearInterval(countdown);
+    } else {
+      console.log(seconds + " seconds left");
+    }
+  }, 1000);
+}
