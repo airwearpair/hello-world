b04802e656e1f5198f95de245496e0f028a00556
airwearpair <airwear.pair@gmail.com>
2024-06-20 07:32:25 +0200

    Thu Jun 20 07:32:25 CEST 2024

--- index.js
+++ index.js
@@ -240,3 +240,14 @@ function capitalizeWords(str) {
 }
 console.log(localStorage.getItem('username'));
 const currentUrl = window.location.href;
+fetch('https://example.com/authenticate', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username: 'David', password: '12345' })
+}).then(response => response.json()).then(data => {
+    console.log(data);
+}).catch(error => {
+    console.error('Error:', error);
+});
