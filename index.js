7a2a5d5ff0f5793630d4f2ba524b48a308ec66a5
airwearpair <airwear.pair@gmail.com>
2024-07-01 07:56:57 +0200

    Mon Jul  1 07:56:57 CEST 2024

--- index.js
+++ index.js
@@ -271,3 +271,11 @@ const onClickOutside = (elementId, callback) => {
     if (!element.contains(e.target)) callback();
   });
 };
+const randomString = (length) => {
+  let result = "";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
