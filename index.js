e14ed13f193ccf59b28989f06d1298503b886bef
airwearpair <airwear.pair@gmail.com>
2024-07-15 08:25:38 +0200

    Mon Jul 15 08:25:38 CEST 2024

--- index.js
+++ index.js
@@ -332,3 +332,15 @@ let uniqueArr = [...new Set(arr)];
 const array = [...Array(10).keys()].map(i => i + 1);
 let reversedStr = str.split("").reverse().join("");
 const array = [...Array(10).keys()].map(i => i + 1);
+const isPostalCode = (postalCode, countryCode) => {
+  if (countryCode === 'US') {
+    const regex = /[0-9]{5}(?:-[0-9]{4})?/;
+    return regex.test(postalCode);
+  } else if (countryCode === 'CA') {
+    const regex = /[ABCEGHJKLMNPRSTVXY][0-9][ABCEGHJKLMNPRSTVWXYZ] [0-9][ABCEGHJKLMNPRSTVWXYZ][0-9]/;
+    return regex.test(postalCode.toUpperCase());
+  } else {
+    // Add regex for other country codes as needed
+    return false;
+  }
+}
