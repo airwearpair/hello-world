3d74a024210bcbd5cb31b029cb30e301116498b9
airwearpair <airwear.pair@gmail.com>
2024-07-22 10:01:29 +0200

    Mon Jul 22 10:01:29 CEST 2024

--- index.js
+++ index.js
@@ -412,3 +412,4 @@ let newArr = oldArr.map(function(item) {
 function randomBetween(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+console.log(localStorage.getItem('username'));
