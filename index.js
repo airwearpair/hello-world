8f89775f7fc383bf8737ce51a113b67fca2bf4b4
airwearpair <airwear.pair@gmail.com>
2024-07-25 08:08:26 +0200

    Thu Jul 25 08:08:25 CEST 2024

--- index.js
+++ index.js
@@ -442,3 +442,11 @@ class Fruit {
 
 }
 const uniqueNums = [...new Set(nums)];
+const randomString = (length) => {
+  let result = "";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
