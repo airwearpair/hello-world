8a2b3e995411c1f7e07ef099c2c84ac1fc902423
airwearpair <airwear.pair@gmail.com>
2024-09-18 09:33:32 +0200

    Wed Sep 18 09:33:32 CEST 2024

--- index.js
+++ index.js
@@ -703,3 +703,11 @@ let randomNum = Math.floor(Math.random() * maxNum);
 function randomBetween(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+const randomString = (length) => {
+  let result = "";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
