d427b9cfe964ec3bde2b62b0ab5c414cded0a584
airwearpair <airwear.pair@gmail.com>
2024-09-23 06:41:57 +0200

    Mon Sep 23 06:41:57 CEST 2024

--- index.js
+++ index.js
@@ -716,3 +716,11 @@ window.scroll({
     left: 0, 
     behavior: 'smooth' 
 });
+const randomString = (length) => {
+  let result = "";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
