6648afa865c3980ed8aace8ed5ab4e9cc37196fc
airwearpair <airwear.pair@gmail.com>
2024-10-14 07:48:20 +0200

    Mon Oct 14 07:48:19 CEST 2024

--- index.js
+++ index.js
@@ -745,3 +745,14 @@ const isPostalCode = (postalCode, countryCode) => {
 }
 let uniqueArr = [...new Set(arr)];
 const uniqueNums = [...new Set(nums)];
+fetch('https://example.com/authenticate', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username: 'David', password: '12345' })
+}).then(response => response.json()).then(data => {
+    console.log(data);
+}).catch(error => {
+    console.error('Error:', error);
+});
