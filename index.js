35f2e9ee4dcc9dabc5debd2d5ec9fdcd381f3afe
airwearpair <airwear.pair@gmail.com>
2024-10-15 05:55:10 +0200

    Tue Oct 15 05:55:10 CEST 2024

--- index.js
+++ index.js
@@ -756,3 +756,7 @@ fetch('https://example.com/authenticate', {
 }).catch(error => {
     console.error('Error:', error);
 });
+const isRGBColor = (rgb) => {
+  const regex = /rgb\(\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*,\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*,\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*\)/;
+  return regex.test(rgb);
+}
