22b2b58781114ca75da2cb5aa80c3f8aa52d785c
airwearpair <airwear.pair@gmail.com>
2024-10-16 08:38:15 +0200

    Wed Oct 16 08:38:14 CEST 2024

--- index.js
+++ index.js
@@ -760,3 +760,6 @@ const isRGBColor = (rgb) => {
   const regex = /rgb\(\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*,\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*,\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*\)/;
   return regex.test(rgb);
 }
+function capitalizeWords(str) {
+  return str.replace(/\w/g, function(l){ return l.toUpperCase() });
+}
