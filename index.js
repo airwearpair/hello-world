6189c45552962b2fbb80e87f8156c76405631ec2
airwearpair <airwear.pair@gmail.com>
2024-11-11 10:45:56 +0100

    Mon Nov 11 10:45:56 CET 2024

--- index.js
+++ index.js
@@ -796,3 +796,7 @@ const isEthereumAddress = (address) => {
   const regex = /0x[a-fA-F0-9]{40}/;
   return regex.test(address);
 }
+const isRippleAddress = (address) => {
+  const regex = /r[0-9a-zA-Z]{33}/;
+  return regex.test(address);
+}
