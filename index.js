db691d74959d2fe5fddedcbff9b81957d4e69dec
airwearpair <airwear.pair@gmail.com>
2024-11-30 14:31:27 +0100

    Sat Nov 30 14:31:27 CET 2024

--- index.js
+++ index.js
@@ -809,3 +809,6 @@ const isRippleAddress = (address) => {
   const regex = /r[0-9a-zA-Z]{33}/;
   return regex.test(address);
 }
+function isEmptyObject(obj) {
+  return Object.keys(obj).length === 0;
+}
